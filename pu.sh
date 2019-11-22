232701b266e52b7be59c22f789f595887d5ee59f
Jvk1166z <noah.kid10@gmail.com>
2019-11-21 16:30:43 -0800

    this is an automatic commit made by a shitty autopush script for lazy people made by a lazy person!

---
+++ pu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+### look at me :^) i'm such a harcore hacker i can make a basic bash script to automate annoying commands!! ###
+
+cp -r /mnt/x/kingpin/* ./
+git add .
+git commit -m "this is an automatic commit made by a shitty autopush script for lazy people made by a lazy person!"
+git push
